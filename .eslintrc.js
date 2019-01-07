06eceddc5e408fda0f6db8f24acbe69ec4b6ded1
sauravyash <yAgasti@gmail.com>
2019-01-08 01:43:53 +1100

    first commit
    helllooooo worldddddddd

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        'no-console': 'off',
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
---
+++ app.js
@@ -0,0 +1,53 @@
+// discord bot for NCSS 2019
+// link: 
+//      https://discordapp.com/oauth2/authorize?client_id=531818322174672907&scope=bot&permissions=1245183424
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const {PythonShell} = require("python-shell")
+let fs = require("fs")
+
+var envJSON = {
+  key: process.env.key
+} // eslint-disable-line
+
+fs.readFile(".env", (err, data) => {
+  if (err) throw err
+  envJSON = JSON.parse(data)
+})
+
+client.on("ready", () => {
+  console.log("Connected as " + client.user.tag)
+})
+
+client.on("message", msg => {
+  // Prevent bot from responding to its own messages
+  if (msg.author == client.user) {
+    return null
+  }
+
+  if (msg.content.startsWith("!")){
+    let _msg = msg.content.substr(1)
+    
+    if (_msg.startsWith("parse")){
+      msg.reply("parsing...")
+        .then(msg => {
+          let code = _msg.slice(6)
+          PythonShell.runString(code, null, function (err, res) {
+            try{
+              msg.delete()
+              if (err) throw err
+              msg.channel.send("RESULTS:")
+              msg.channel.send(res)
+            } catch(e) {
+              msg.channel.send(`ERROR WHEN PARSING: ${e}`)
+            }
+          })
+        })
+      
+    }
+  }
+})
+
+let bot_secret_token = envJSON.key || "NTMxODE4MzIyMTc0NjcyOTA3.DxTeOw.Hf6xMW_iJ3vdbreNSpz2uPZ7dlc"
+
+client.login(bot_secret_token)
