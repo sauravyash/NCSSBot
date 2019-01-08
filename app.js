74d61126b6b1e439252cbb90bc6a7e81dadff721
sauravyash <yAgasti@gmail.com>
2019-01-09 00:33:31 +1100

    SECURITY PATCH

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 //      https://discordapp.com/oauth2/authorize?client_id=531818322174672907&scope=bot&permissions=1245183424
 const Discord = require("discord.js")
 const client = new Discord.Client()
-const {PythonShell} = require("python-shell")
+let Python = require("python-runner")
 const readJsonSync = require("read-json-sync")
 
 let env
@@ -16,6 +16,36 @@ if (!process.env.key){
   } 
 }
 
+let parsePython = msg => {
+  let _msg = msg.content.substr(1)
+  if (_msg.startsWith("parse python =>")){
+    msg.reply("parsing...")
+      .then(msg => {
+        let code = _msg.slice(15)
+        let options = {
+          env: {
+            key: "You Think YOUR'E KYOOL????"
+          }
+        }
+        Python.exec(code, options).then( res =>{
+          try{
+            msg.delete()
+            msg.reply("RESULTS:")
+            console.log(res)
+            
+            if (res) msg.channel.send(res)
+            else msg.channel.send("ERROR: Python Code EMPTY stdout")
+          } catch(e) {
+            msg.channel.send(`ERROR WHEN PARSING: ${e}`)
+          }
+        }).catch((err)=>{
+          msg.channel.send(`ERROR: ${err}`)
+        })
+      })
+  }
+}
+
+
 client.on("ready", () => {
   console.log("Connected as " + client.user.tag)
 })
@@ -27,26 +57,7 @@ client.on("message", msg => {
   }
 
   if (msg.content.startsWith("!")){
-    let _msg = msg.content.substr(1)
-    
-    if (_msg.startsWith("parse python =>")){
-      msg.reply("parsing...")
-        .then(msg => {
-          let code = _msg.slice(15)
-          PythonShell.runString(code, null, function (err, res) {
-            try{
-              msg.delete()
-              if (err) throw err
-              msg.channel.send("RESULTS:")
-              if (res) msg.channel.send(res)
-              else msg.channel.send("ERROR: Python Code Not In Valid Format")
-            } catch(e) {
-              msg.channel.send(`ERROR WHEN PARSING: ${e}`)
-            }
-          })
-        })
-      
-    }
+    parsePython(msg)
   }
 })
 
