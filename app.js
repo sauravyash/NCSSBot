1c122f928e47d5b659753e0d8888a62968a262dd
sauravyash <yAgasti@gmail.com>
2019-01-15 16:05:04 +1100

    added message to new member msg

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ client.on('guildMemberAdd', member => {
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
+  channel.send(`Welcome to the server, ${member}. Please type \`!nickname\` to verify yourself. `);
 });
 
 // THE REAL MAGIC
