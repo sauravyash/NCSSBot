bdfede02d1e696baaa9f6ff08e3ab3492f0b91ae
sauravyash <yAgasti@gmail.com>
2019-01-15 16:48:49 +1100

    fix break

--- bot_modules/nickname.js
+++ bot_modules/nickname.js
@@ -5,22 +5,25 @@ function nickname(msg, arg) {
   arg = arg.split(' ')
   if (!msg.member.nickname) {
     if (!arg[0] || !arg[1] || !arg[2]) {
-      msg.reply("Please use the arguments: [First Name, Last Name, Group Number]")
+      msg.reply("Please write the command in the following format.\n\`!nickname {First Name} {Last Name} {Group Number}\`")
+      //Please use the arguments: [First Name, , Group Number]
     } else {
-      msg.member.setNickname(arg[0] + " " + arg[1].split('')[0] + " [" + arg[2] + "]")
+      // msg.member.setNickname(arg[0] + " " + arg[1].split('')[0] + " [" + arg[2] + "]")
+      msg.member.setNickname(`${arg[0]} ${arg[1].split[0]} [${arg[2]}]`)
       msg.reply("Initial Name Successfully Set")
     }
-  } else {
+  }
+  // underneath here
+  else {
     if (arg[0] === 'reset') {
-      msg.member.setNickname(msg.member.nickname.split(' : ')[1])
+      msg.member.setNickname(msg.member.nickname.split(' : ')[msg.member.nickname.split(' : ').length - 1])
       msg.reply("Nickname Successfully Reset")
     } else if (!arg[0]) {
-      msg.reply("Please write the command in the following format.\n`!nickname {nickname} || reset")
+      msg.reply("Please write the command in the following format.\n\`!nickname {nickname} || reset\`")
     } else {
-      msg.member.setNickname(arg[0] + " : " + msg.member.nickname)
+      msg.member.setNickname(arg.join(" ").trim() + " : " + msg.member.nickname.split(' : ')[msg.member.nickname.split(' : ').length - 1])
       msg.reply("Custom Nickname Successfully Set")
     }
   }
 }
-
 module.exports = nickname
