bc4ad3f0a9f41ae89b12bfef205622bd5dcc42cd
sauravyash <yAgasti@gmail.com>
2019-01-15 13:10:04 +1100

    progress on nickname stuff

--- bot_modules/nickname.js
+++ bot_modules/nickname.js
@@ -1,4 +1,20 @@
-function nickname() {
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+function nickname(msg, arg) {
+
+  // !username lolwot
+  // nickname (Yash A [9])
+  console.log(msg.author.nickname);
+  msg.member.setNickname('Cool Name')
+    .then(console.log)
+    .catch(console.error);
+
+  if (!msg.member.nickname) {
+    msg.reply("Test No nickname" + msg.author.username + msg.member.nickname)
+  } else {
+    msg.reply("Test nickname" + msg.author.username + msg.member.nickname)
+  }
 
 }
 
